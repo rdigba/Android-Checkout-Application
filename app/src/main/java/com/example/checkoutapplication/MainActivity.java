c9b8501f1448bbc94e8af4a42efffcb739f0afe8
ruemu-digba <ruemu.digba@mail.utoronto.ca>
2020-10-06 16:20:52 -0400

    Added names to strings.xml, and added functionality to one button so the user can click the button to add 1 burger to their cart.

--- app/src/main/java/com/example/checkoutapplication/MainActivity.java
+++ app/src/main/java/com/example/checkoutapplication/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.checkoutapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TextViews for each foods, and totalText which is the price we will calculate
+    private TextView numBurgersText, numFriesText, numDrinksText, totalText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addBurger();
+    }
+
+    private void addBurger() {
+
+        numBurgersText = findViewById(R.id.burgerCountTextView);
+        // Getting button
+        Button addBurgerButton = findViewById(R.id.addBurgerButton);
+        // What happens when user clicks button
+        addBurgerButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                // numBurgers will increase by 1
+                int numBurgers = Integer.parseInt(numBurgersText.getText().toString());
+                numBurgers++;
+                numBurgersText.setText(Integer.toString(numBurgers));
+            }
+        });
     }
 }
